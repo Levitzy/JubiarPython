1cd5eadaccfd2df3963d6f09372e4b477129ecd6
Levitzy <levitzy123@gmail.com>
2024-11-10 19:53:49 +0800

    nm fixed

--- api/adminCheck.py
+++ api/adminCheck.py
@@ -1,4 +1,4 @@
 def is_admin(user_id):
     # Replace with actual admin verification logic
-    admin_ids = ["1234567890"]  # List of admin user IDs
+    admin_ids = ["8565063196874531"]  # List of admin user IDs
     return user_id in admin_ids
